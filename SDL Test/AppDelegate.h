2c66e93ef93fc1ebca50fc83ade526517dc386f3
Thiago Leandro Liporace <85422792+thiliporace@users.noreply.github.com>
2024-12-18 16:57:44 -0300

    Initial Commit

---
+++ SDL Test/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  SDL Test
+//
+//  Created by Thiago Liporace on 18/12/24.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ SDL Test/AppDelegate.m
@@ -0,0 +1,32 @@
+//
+//  AppDelegate.m
+//  SDL Test
+//
+//  Created by Thiago Liporace on 18/12/24.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (strong) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+- (BOOL)applicationSupportsSecureRestorableState:(NSApplication *)app {
+    return YES;
+}
+
+
+@end
